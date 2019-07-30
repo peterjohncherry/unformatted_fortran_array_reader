6c449444fd8e5d1fcda2fb1fc20021639d9fd7e1
peterjohncherry <peter.cherry@northwestern.edu>
2019-07-30 15:49:25 +0200

    Tool to read unformatted Fortran arrays and locate nonzero elements

---
+++ mat_reader.py
@@ -0,0 +1,94 @@
+import sys
+import os
+import numpy as np
+from scipy.io import FortranFile
+
+def read_binary_fortran_file(name, datatype, dim0, dim1=1 ):
+
+    input_array=np.ndarray((dim0*dim1))
+
+    if dim1 == 1 :
+        fmat = FortranFile(name, 'r')
+        if (datatype == "real"):
+            input_array = fmat.read_reals(dtype=np.float64)
+        if (datatype == "int"):
+            input_array = fmat.read_reals(dtype=np.int32)
+        fmat.close()
+
+    else :
+      if (datatype == "real"):
+          fmat = FortranFile(name, 'r')
+          input_array = fmat.read_reals(dtype=np.float64)
+          input_array = input_array.reshape((dim0,dim1)).transpose()
+          fmat.close()
+
+      elif (datatype == "int"):
+          fmat = FortranFile(name, 'r')
+          input_array = fmat.read_ints(dtype=np.int16)
+          input_array = input_array.reshape((dim0,dim1)).transpose()
+          fmat.close()
+
+      elif ( datatype == "complex" ):
+          sys.exit("reading of complex matrices is not directly possible, must write out real and imag parts " \
+                   "as two seperate real matrices\n")
+
+      else :
+          sys.exit("reading of datatype \"" +  datatype + "\" is not implemented\n ")
+
+    return input_array
+
+#reads the matrix info file which is generated to aid reading of fortran binary file
+def read_array_info_file(name):
+
+    infofile = open(name,"r")
+    for line in infofile.readlines():
+        line_array = line.split()
+        if line_array[0] == "dim0":
+            dim0 = int(line_array[2])
+        elif line_array[0] == "dim1":
+            dim1 = int(line_array[2])
+        elif line_array[0] == "datatype":
+            datatype = str(line_array[2])
+
+    return dim0, dim1, datatype
+
+def read_fortran_array(seedname):
+    nrows, ncols, datatype = read_array_info_file(seedname+".info")
+
+    if datatype == "complex" :
+        name =seedname+"_real.bin"
+        array_real =  read_binary_fortran_file(name, "real", nrows, ncols)
+        name = seedname+"_imag.bin"
+        array_imag = read_binary_fortran_file( name, "real", nrows, ncols)
+        return array_real + 1j*array_imag
+
+    elif datatype == "real" :
+        name = seedname+".bin"
+        array_real =  read_binary_fortran_file(name, datatype, nrows, ncols)
+        return array_real
+
+    elif datatype == "int" :
+        name =seedname+".bin"
+        array_ints =  read_binary_fortran_file(name, datatype, nrows, ncols)
+        return array_ints
+
+    else :
+        sys.exit("Not implemented " + datatype + " ABORTING" )
+
+def find_nonzero_elems(seedname, input_array, threshold = 1e-10):
+    non_zero_ids = np.argwhere(np.abs(input_array) > threshold )
+    non_zero_elems = input_array[non_zero_ids]
+
+#    np.savetxt(seedname+"_nonzero_ids.txt", non_zero_ids, fmt ='%.4i')
+#    np.savetxt(seedname+"_nonzero_elems.txt", non_zero_elems)
+ 
+    outfile = open (seedname+"_nonzero.txt","w+" )
+    for idx in non_zero_ids :
+        outfile.write(str(idx)+" =  " + str(input_array[idx]) + "\n" )
+
+    outfile.close()
+        
+ 
+    
+def read_numpy_array(name):
+    return np.load(name)
---
+++ read_fortran_array_sequence.py
@@ -0,0 +1,95 @@
+#python libraries
+import glob
+import os
+import sys
+import argparse
+
+#external libraries
+import numpy as np
+import scipy as sp
+
+#own libraries
+import mat_reader as mr
+
+def get_seedname_list(base_name):
+    seedname_list = []
+    for infile in sorted(glob.glob(base_name+'*')):
+        if infile.endswith('.info'):
+                seedname = infile[:-5]
+                seedname_list.append(seedname)
+    return seedname_list
+
+def read_array_sequence(basename, save_as_numpy_file = True, save_as_text_file=True, find_nonzero_elems=True, threshold = 1e-10):
+    #dir_path = "/home/peter/RS_FILES/"
+    seedname_list = get_seedname_list(basename)
+
+    for seedname in seedname_list:
+        print("reading" + seedname)
+        input_array = mr.read_fortran_array(seedname)
+
+        if save_as_numpy_file:
+            np.save(seedname, input_array)
+
+        if save_as_text_file:
+            np.savetxt(seedname+".txt", input_array)
+        
+        if find_nonzero_elems :
+            mr.find_nonzero_elems(seedname, input_array,threshold)
+
+usage = 'read_fortran_array_sequence --basename=basename'
+
+parser = argparse.ArgumentParser( usage, formatter_class=argparse.ArgumentDefaultsHelpFormatter )
+
+parser.add_argument('--basename',
+                   action='store',
+                   type=str,
+                   dest='basename',
+                   default=None,
+                   help="the relevant files can be found at {target_directory}+/+{basename}+*"\
+                    "target_directory defaults to current directory if not set")
+
+parser.add_argument('--target_directory',
+                   action='store',
+                   type=str,
+                   dest='target_directory',
+                   default=os.getcwd() + '/',
+                   help="the relevant files can be found at {working_directory}+/+{basename}+*")
+
+parser.add_argument('--save_as_npy',
+                   action='store',
+                   type=bool,
+                   dest='save_as_npy',
+                   default=True,
+                   help="Save matrices as npy files")
+
+parser.add_argument('--save_as_text',
+                   action='store',
+                   type=bool,
+                   dest='save_as_text',
+                   default=True,
+                   help="Save matrices as human readable text files")
+
+parser.add_argument('--find_nonzero_elems',
+                   action='store',
+                   type=bool,
+                   dest='find_nonzero_elems',
+                   default=True,
+                   help="find nonzero elements and output values and indexes into text file")
+
+parser.add_argument('--threshold',
+                   action='store',
+                   type=float,
+                   dest='threshold',
+                   default=1e-10,
+                   help="Threshold for identifying nonzero elements")
+
+
+args = parser.parse_args(sys.argv[1:])
+
+basename = str(args.target_directory)+str(args.basename)
+print("basename = ", basename)
+save_as_npy = bool(args.save_as_npy)
+save_as_text = bool(args.save_as_text)
+find_nonzero_elems = bool(args.save_as_text)
+threshold = np.float64(args.threshold)
+read_array_sequence(basename, save_as_npy, save_as_text, find_nonzero_elems, threshold)
